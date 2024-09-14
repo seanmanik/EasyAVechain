25ed0cc79fb7bbfd18b6317c357653dee18534de
seanmanik <seanmanik@gmail.com>
2024-09-14 12:53:54 +0800

    Add contracts updated

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,21 +1,20 @@
-import "@nomicfoundation/hardhat-toolbox"
-import "@vechain/sdk-hardhat-plugin"
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@vechain/sdk-hardhat-plugin";
+import '@vechain/sdk-hardhat-plugin'
 import "@openzeppelin/hardhat-upgrades";
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
 
-const accounts = ['0x2422eb37a0046d42e3c8d05c7d972de7fe1bb805e90b3a0dbc7d12b4d444c634']
+const accounts = ['f519df21ea7ad71c5c7ea63d5123732525b1dbbc6ea3f03b7d440916ad80f6f0'];
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     vechain_testnet: {
       url: "https://testnet.vechain.org",
       accounts,
-
-      // optionally use fee delegation to let someone else pay the gas fees
-      // visit vechain.energy for a public fee delegation service
+      // optionally use fee delegation
       delegator: {
         delegatorUrl: "https://sponsor-testnet.vechain.energy/by/90"
       },
@@ -23,7 +22,9 @@ module.exports = {
     },
     vechain_mainnet: {
       url: "https://mainnet.vechain.org",
-      accounts
+      accounts,
     },
-  }
+  },
 };
+
+export default config;
